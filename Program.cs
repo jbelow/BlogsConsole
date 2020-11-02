2c22520097d2a83e86f0e77ffc557862699ba0d5
jbelow <joshbelow@gmail.com>
2020-11-02 11:42:58 -0600

    making switch and moving choice 1 and 2

--- Program.cs
+++ Program.cs
@@ -13,32 +13,63 @@ namespace BlogsConsole
         {
             logger.Info("Program started");
 
-            try
+            int choice = 0;
+            
+            
+            var db = new BloggingContext();
+
+            while (choice != -1){
+                Console.WriteLine("1) Display all blogs \n2) Add Blog \n3) Create Post \n4) Display Posts\n-1) exit the program");
+                choice = Int32.Parse(Console.ReadLine());
+
+
+                switch (choice)
             {
+                case 1:
+                    // Display all Blogs from the database
+                    var query = db.Blogs.OrderBy(b => b.Name);
 
-                // Create and save a new Blog
-                Console.Write("Enter a name for a new Blog: ");
-                var name = Console.ReadLine();
+                    Console.WriteLine("All blogs in the database:");
+                    foreach (var item in query)
+                    {
+                        Console.WriteLine(item.Name);
+                    }
+                    break;
 
-                var blog = new Blog { Name = name };
+                case 2:
+                    try
+                    {
+                        // Create and save a new Blog
+                        Console.Write("Enter a name for a new Blog: ");
+                        var name = Console.ReadLine();
 
-                var db = new BloggingContext();
-                db.AddBlog(blog);
-                logger.Info("Blog added - {name}", name);
+                        var blog = new Blog { Name = name };
 
-                // Display all Blogs from the database
-                var query = db.Blogs.OrderBy(b => b.Name);
+                        db.AddBlog(blog);
+                        logger.Info("Blog added - {name}", name);
 
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
-                {
-                    Console.WriteLine(item.Name);
-                }
+                        
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Error(ex.Message);
+                    }
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+
             }
-            catch (Exception ex)
-            {
-                logger.Error(ex.Message);
             }
+            
+
+
 
             logger.Info("Program ended");
         }
