21364badfcc6f7889e6d483938d74009123bf6c0
jbelow <joshbelow@gmail.com>
2020-11-05 21:43:31 -0600

    removed a space

--- Post.cs
+++ Post.cs
@@ -5,7 +5,6 @@ namespace BlogsConsole
         public int PostId { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
-
         public int BlogId { get; set; }
         public Blog Blog { get; set; }
     }
